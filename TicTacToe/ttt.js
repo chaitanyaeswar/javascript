3e58641f72539d7393b5f801c2516ee5922561f5
Chaitanya <chaitanya@veooz.com>
2015-11-24 14:28:45 +0530

    bin search

--- TicTacToe/ttt.js
+++ TicTacToe/ttt.js
@@ -1,10 +1,14 @@
-var X = true; 
+var X = true;
+var xScore = 0;
+var oScore = 0;
+var winningScores;
 
 $(document).ready(initialize);
 
 function initialize() {
     addEvent();
-    var winnigScores = generateWinnerScoresArray();
+    winningScores = generateWinnerScoresArray();
+    console.log(winningScores);
 }
 
 function addEvent() {
@@ -13,13 +17,25 @@ function addEvent() {
         if(id >=0 && id <= 9) {
             console.log($(this).attr("id"));
             
+            var isWinnerAvailable = false;
+            var winner = ""
             if(X) {
                 X = false;
                 $(this).addClass("selectedX");
+                xScore += Math.pow(2, id);
+                isWinnerAvailable = checkWinner(true);
+                winner = "X"
             }
             else {
                 X = true;
                 $(this).addClass("selectedO");
+                oScore += Math.pow(2, id);
+                isWinnerAvailable = checkWinner(false);
+                winner = "O"
+            }
+            
+            if(isWinnerAvailable) {
+                window.alert(("Hey " + winner + " won :)"));
             }
         }
     });
@@ -57,15 +73,34 @@ function generateWinnerScoresArray() {
         counter+=2;
     }
     
-    console.log(winningScores);
+//    console.log(winningScores);
     winningScores.sort(function(a, b) {
         return (a - b);
     });
     
-    console.log(winningScores);
+//    console.log(winningScores);
     return winningScores;
 }
 
-function checkWinner() {
+function checkWinner(isXMove) {
+    console.log(winningScores + " " + xScore + " " + oScore);
+    var index = -1;
+    if(isXMove)
+        index = binarySearch(xScore, 0, winningScores.length);
+    else
+        index = binarySearch(oScore, 0, winningScores.length);
     
+    if(index != -1) return true;
+    
+    return false;
+}
+
+function binarySearch(score, start, end) {
+    if(start == (end - 1)) return -1;
+    
+    var mid = parseInt((start + end) / 2);
+    
+    if(winningScores[mid] == score) return mid;
+    else if(winningScores[mid] > score) return binarySearch(score, start, mid);
+    else if(winningScores[mid] < score) return binarySearch(score, mid, end);
 }
